bfc96161894c204b21fdb4a5ebd364c284a66387
Anurag Yadav <anurag@Anurags-MacBook-Pro.local>
2021-09-07 16:42:40 +0530

    Removed old demos dir from the repository

--- demos/security/gcp-startup-script.sh
+++
@@ -1,3 +0,0 @@
-export HOST_NAME=$(curl -s http://metadata.google.internal/computeMetadata/v1/instance/name -H Metadata-Flavor:Google)
-cd /home/anurag_yadav/minisys/demos/security
-dc up -d $HOST_NAME
