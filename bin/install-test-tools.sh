4ba40a9c2f8241cae66bfbf7d6a51724f5d69ba5
Anurag Yadav <anurag@Anurags-MacBook-Pro.local>
2022-04-03 18:57:03 +0530

    Added django version to django install in install-test-tools.sh

--- bin/install-test-tools.sh
+++ bin/install-test-tools.sh
@@ -19,8 +19,11 @@ echo "-- Install python --"
 sudo apt-get install -y python3-pip
 verify_success $? python3-pip
 
-sudo pip3 install django gunicorn python-json-logger
+sudo pip3 install "django>=3.2,<4" gunicorn python-json-logger 
 verify_success $? "django and gunicorn"
+
+sudo pip3 install opentracing jaeger-client django-prometheus
+verify_success $? "jaeger tracing and prometheus"
 
 echo "-- Install PostgreSQL --"
 sudo apt-get install -y postgresql
